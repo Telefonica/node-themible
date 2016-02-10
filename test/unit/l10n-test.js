8ad25342180137423793b3d228c2934bd0476510
labajo <labajo@tid.es>
2016-02-10 11:34:17 +0100

    ADD default locale parameter

--- test/unit/l10n-test.js
+++ test/unit/l10n-test.js
@@ -33,6 +33,33 @@ describe('i10n Tests', function() {
     expect(i18Stub.initCalled).to.be.true;
   });
 
+  it('should call i10nMiddleware with locale', function() {
+    var i18Stub = {
+      called: false,
+      configure: function(options) {
+        expect(options.updateFiles).to.be.false;
+        expect(options.locales).to.be.equal('locales');
+        expect(options.directory).to.be.equal('directory');
+        expect(options.objectNotation).to.be.true;
+        expect(options.defaultLocale).to.be.equal('defaultLocale');
+        this.called = true;
+      },
+      initCalled: false,
+      init: function(req, res, next) {
+        this.initCalled = true;
+        return 'init';
+      }
+    };
+
+    var i10nMiddleware = proxyquire('../../lib/l10n', {
+      'i18n': i18Stub
+    });
+
+    i10nMiddleware('locales', 'directory', 'defaultLocale')();
+    expect(i18Stub.called).to.be.true;
+    expect(i18Stub.initCalled).to.be.true;
+  });
+
   it('should call i10nMiddleware without directory', function() {
     var i18Stub = {
       called: false,
